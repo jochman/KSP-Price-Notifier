30f42b47597d360812d0a04fbe38095d729b131d
Bar Hochman <bhochman@paloaltonetworks.com>
2022-03-29 19:18:27 +0300

    build: add tests

--- tests/test_get_price.py
+++ tests/test_get_price.py
@@ -1,10 +1,15 @@
-class providerMock:
-    @staticmethod
-    def get(*args, **kwargs):
-        return {'result': {'data': {'price': 670}}}
+from collections import namedtuple
 
 
-def find_price_test():
+
+def test_get_price(requests_mock):
     from ksp_price_notifier import GetPriceFromKSP
-    getter = GetPriceFromKSP('', providerMock)
+    import requests
+    requests_mock.get('https://ksp.co.il/m_action/api/item/187019', json={'result': {'data': {'price': 670}}})
+    getter = GetPriceFromKSP(provider=requests)
+    assert getter.get_price_from_ksp(187019) == 670
+
+def test_api_is_alive():
+    from ksp_price_notifier import GetPriceFromKSP
+    getter = GetPriceFromKSP()
     getter.get_price_from_ksp(187019)
