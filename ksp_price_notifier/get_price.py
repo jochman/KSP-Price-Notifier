9aa64af186a55a682d7e645053922cce0b48cfe8
Bar Hochman <bhochman@paloaltonetworks.com>
2022-03-29 17:59:52 +0300

    fix: url in GetPRices

--- ksp_price_notifier/get_price.py
+++ ksp_price_notifier/get_price.py
@@ -4,7 +4,8 @@ import requests
 class GetPriceFromKSP:
     def __init__(self, url='https://ksp.co.il/m_action/api/item/', provider=requests) -> None:
         self._provider = provider
+        self._url = url
 
     def get_price_from_ksp(self, uin: int) -> int:
-        resp = self._provider.get(f'https://ksp.co.il/m_action/api/item/{uin}')
+        resp = self._provider.get(f'{self._url.rstrip("/")}/{uin}')
         return int(resp.json()['result']['data']['price'])
