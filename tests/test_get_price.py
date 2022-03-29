2138a5b8aeb2f166b43e03bd6c8f5a7a9739c9b1
Bar Hochman <bhochman@paloaltonetworks.com>
2022-03-29 21:06:18 +0300

    fix tests

--- tests/test_get_price.py
+++ tests/test_get_price.py
@@ -1,14 +1,14 @@
 def test_get_price(requests_mock):
     import requests
 
-    from ksp_price_notifier import GetPriceFromKSP
+    from ksp_price_notifier import KSP
     requests_mock.get('https://ksp.co.il/m_action/api/item/187019',
                       json={'result': {'data': {'price': 670}}})
-    getter = GetPriceFromKSP(provider=requests)
-    assert getter.get_price_from_ksp(187019) == 670
+    getter = KSP(provider=requests)
+    assert getter.get_price(187019) == 670
 
 
 def test_api_is_alive():
-    from ksp_price_notifier import GetPriceFromKSP
-    getter = GetPriceFromKSP()
-    getter.get_price_from_ksp(187019)
+    from ksp_price_notifier import KSP
+    getter = KSP()
+    getter.get_price(187019)
