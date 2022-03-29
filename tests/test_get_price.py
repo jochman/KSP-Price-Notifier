cd09369c4872a0ca65b5fe596465e2e5c45c7051
Bar Hochman <bhochman@paloaltonetworks.com>
2022-03-29 19:21:34 +0300

    fix: pre-commit

--- tests/test_get_price.py
+++ tests/test_get_price.py
@@ -1,13 +1,12 @@
-from collections import namedtuple
-
-
-
 def test_get_price(requests_mock):
-    from ksp_price_notifier import GetPriceFromKSP
     import requests
-    requests_mock.get('https://ksp.co.il/m_action/api/item/187019', json={'result': {'data': {'price': 670}}})
+
+    from ksp_price_notifier import GetPriceFromKSP
+    requests_mock.get('https://ksp.co.il/m_action/api/item/187019',
+                      json={'result': {'data': {'price': 670}}})
     getter = GetPriceFromKSP(provider=requests)
     assert getter.get_price_from_ksp(187019) == 670
+
 
 def test_api_is_alive():
     from ksp_price_notifier import GetPriceFromKSP
