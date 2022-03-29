0a21a6831a017f0e72a92404468328faeec6a7e2
Bar Hochman <bhochman@paloaltonetworks.com>
2022-03-29 17:53:25 +0300

    gitignore

--- ksp_price_notifier/price-notifier.py
+++ ksp_price_notifier/price-notifier.py
@@ -1,6 +1,8 @@
+from pathlib import Path
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import argparse
+import typer 
 
 def build_options():
     chrome_options = webdriver.ChromeOptions()
@@ -22,18 +24,24 @@ def get_price(url: str, chrome_driver: str) -> int:
     else:
         raise KeyError('Could not find price or the product')
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Give uid and price, will notify if the price is lower than wanted price"
-    )
-    parser.add_argument('uin', type=int, help="the uin of the product, can be found in the url.")
-    parser.add_argument('target_price', type=int, help="Target price. if the price is below, will let you know.")
-    parser.add_argument('--chromedriver', help="Path to the chromedriver")
-    args = parser.parse_args()
-    url = url = f"https://ksp.co.il/?uin={args.uin}"
-    price = get_price(url, args.chromedriver)
-    if int(price) <= args.target_price:
+
+def main(uin: int, target_price: int, chromedriver: Path):
+    """"Give uid and price, will notify if the price is lower than wanted price
+
+    Args:
+        uin (int): the uin of the product, can be found in the url.
+        target_price (int): Target price. if the price is below, will let you know.
+        chromedriver (Path): Path to the chromedriver
+    """
+
+    url = url = f"https://ksp.co.il/?uin={uin}"
+    price = get_price(url, chromedriver)
+    if int(price) <= target_price:
         print(f"The price is lower than the target price, it is now {price}")
         print(f"Go and buy! {url}")
     else:
         print("The price is higher than target price.")
+
+if __name__ == "__main__":
+    typer.run(main)
+
