a5adec1467910fc2c61702dcffc48ddac1582c67
Bar Hochman <bhochman@paloaltonetworks.com>
2022-03-29 20:09:12 +0300

    version fix

--- ksp_price_notifier/__init__.py
+++ ksp_price_notifier/__init__.py
@@ -2,4 +2,4 @@ import pkg_resources
 
 from .get_price import GetPriceFromKSP as GetPriceFromKSP  # noqa: F401
 
-__version__ = pkg_resources.get_distribution("packagename").version
+__version__ = pkg_resources.get_distribution("ksp_price_notifier").version
